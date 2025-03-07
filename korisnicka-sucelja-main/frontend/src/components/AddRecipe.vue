1d00a15dde4fc0d2bd72e0cefbd71bbe6a3fa29d
ivanivso7 <ivan2filipovic111@gmail.com>
2025-03-07 10:31:01 +0100

    Initial commit

---
+++ korisnicka-sucelja-main/frontend/src/components/AddRecipe.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="h-[30rem] bg-yellow-300 flex justify-center items-center">
+    <h1 class="text-4xl font-bold">Add Recipe</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  height: 15vh;
+}
+</style>
